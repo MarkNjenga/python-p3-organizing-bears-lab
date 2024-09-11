99eb9b3f64ff66ace7e05e953e3368ec3b339253
Mark Njenga <marknjenga25@gmail.com>
2024-09-11 15:29:15 +0300

    "Added create and insert SQL scripts for bears table, and implemented five SQL queries in python file."

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears(id, name, age, sex, color, temperament, alive)
+VALUES
+(1, "Mr. Chocolate", 2, "M", "Brown", "choleric", True),
+(2, "Rowdy", 6, "M", "Black", "melancholic", True),
+(3, "Tabitha", 4, "F", "White", "choleric", True),
+(4, "Sergeant Brown", 8, "M", "Grey", "sanguine", False),
+(5, "Melissa", 3, "F", "Brown", "melancholic", False),
+(6, "Grinch", 7, "M", "White", "choleric", True),
+(7, "Wendy", 5, "F", "Orange", "sanguine", True),
+(8, NULL, 2, "F", "Black", "melancholic", False);
